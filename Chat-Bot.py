c2a3cedf5374a2864215dff4e7e7d7cc99adda76
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-09-23 14:23:22 -0700

    Final update for Chat-Bot.py

--- Chat-Bot.py
+++ Chat-Bot.py
@@ -1,3 +1,4 @@
+import random
 
 def get_bot_response(user_response):
 
@@ -5,11 +6,11 @@ def get_bot_response(user_response):
     chat_bot_response_sad = ["I'm sorry to hear that.", "Well, hopefully your day gets better", "Lifting weights will make you feel better."]
 
     if user_response == ["Happy", "Good", "Great", "Awesome", "Amazing"]:
-        return chat_bot_response_happy
+        return print(random.choice(chat_bot_response_happy))
     elif user_response == ["Sad", "Down", "Not great", "Not good", "Not well", "Upset"]:
-        return chat_bot_response_sad
+        return print(random.choice(chat_bot_response_sad))
     else:
-        return "I hope your day imporves even more"
+        return print("I hope your day imporves even more")
 
 
     print("Welcome to Chat Bot")
