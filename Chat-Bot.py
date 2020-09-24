c4ea5c9536dcdf14bead7bb2770e08a36fe9e8ac
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-09-23 22:33:33 -0700

    Update for Chat-Bot.py

--- Chat-Bot.py
+++ Chat-Bot.py
@@ -1,38 +1,32 @@
-import random
+from random import choice
 
 def get_bot_response(user_response):
+  
+  chat_bot_response_happy = ["Noice!", "Happy to hear that!", "Awesome!"]
+  chat_bot_response_sad = ["I'm sorry to hear that.", "Well, hopefully your day gets better", "Lifting weights will make you feel better."]
 
-    chat_bot_response_happy = ["Noice!", "Happy to hear that!", "Awesome!"]
-    chat_bot_response_sad = ["I'm sorry to hear that.", "Well, hopefully your day gets better", "Lifting weights will make you feel better."]
+  happy_response = ["Happy", "Good", "Great", "Awesome", "Amazing"]
+  sad_response = ["Sad", "Down", "Not great", "Not good", "Not well", "Upset"] 
 
-    happy_list = ["Happy", "Good", "Great", "Awesome", "Amazing"]
-    sad_list = ["Sad", "Down", "Not great", "Not good", "Not well", "Upset"]
-
-    if user_response in happy_list:
-        return print(random.choice(chat_bot_response_happy))
-    elif user_response in sad_list:
-        return print(random.choice(chat_bot_response_sad))
-    else:
-        return print("I hope your day imporves even more")
-
-
-    print("Welcome to Chat Bot")
-    print("Please enter how you are feeling today")
+  if user_response == happy_response:
+    return choice(chat_bot_response_happy)
+  elif user_response == sad_response:
+    return choice(chat_bot_response_sad)
+  else:
+    return print("I hope your day imporves even more")
 
 
-    user_response = ""
+
+print("Welcome to Chat Bot")
+print("Please enter how you are feeling today")
+
+user_response = ""
     
-    while True:
-        user_response = input("How are you feeling today?: ")
+while True:
+  user_response = input("How are you feeling today? ")
 
-        if user_response == ["done", "stop it", "quit"]:
-            break
-
-        bot_response = get_bot_response(user_response)
-        print(bot_response)
-
-
-get_bot_response("Not good")
-get_bot_response("Great")
-get_bot_response("Awesome")
-get_bot_response("IDK")
+  if user_response == ["Done", "Stop It", "Quit", "Quit It", "Stop"]:
+    break
+        
+bot_response = get_bot_response(user_response)
+print(bot_response)
